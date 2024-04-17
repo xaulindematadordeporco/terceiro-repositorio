ec4d87325ddd16787eb1ad9f5d00f39b287f7a72
xaulindematadordeporco <106355661+xaulindematadordeporco@users.noreply.github.com>
2024-04-16 22:32:38 -0300

    amo a Evelyn

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
+for (let i=0; i<botoes.length; i++) {
+    
+    botoes[i].onclick = function(){
 
-        for (let j = 0; j < botoes.length; j++) {
+        for (let j=0; j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -15,10 +17,12 @@ for (let i = 0; i < botoes.length; i++) {
 
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-05-10T23:59:59");
+const tempoObjetivo1 = new Date("2024-04-30T23:59:59");
+const tempoObjetivo2 = new Date("2024-05-10T23:59:59");
+const tempoObjetivo3 = new Date("2024-06-15T23:59:59");
+const tempoObjetivo4 = new Date("2024-12-13T23:59:59");
 
-
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -31,7 +35,7 @@ function calculaTempo(tempoObjetivo) {
 
     segundos %= 60;
     minutos %= 60;
-    horas %24= 24;
+    horas %= 24;
 
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 }
