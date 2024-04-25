fcc82962a2db30c46d20542c42e47a968e6672b0
xaulindematadordeporco <106355661+xaulindematadordeporco@users.noreply.github.com>
2024-04-25 19:06:55 -0300

    amo a evelyn

--- main.js
+++ main.js
@@ -18,11 +18,13 @@ for (let i=0; i<botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-04-30T23:59:59");
-const tempoObjetivo2 = new Date("2024-05-23T23:59:59");
+const tempoObjetivo2 = new Date("2024-05-10T23:59:59");
 const tempoObjetivo3 = new Date("2024-06-15T23:59:59");
 const tempoObjetivo4 = new Date("2024-12-13T23:59:59");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
+
+const situacoes = document.querySelectorAll(".situacao");
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -46,6 +48,33 @@ function calculaTempo(tempoObjetivo) {
     
 }
 
+function calculaSituacao(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    if (tempoFinal > 0) {
+        return " Ainda há tempo ";
+    } else {
+        return "Perdeu!!!";
+        
+    }
+    
+}
+
+function atualizaSituacao () {
+    for (let i = 0; i < situacoes.length; i++) {
+        situacoes[i].textContent = calculaSituacao(tempos[i]);
+    }
+}
 
 function atualizaCronometro() {
     for ( let i = 0; i < contadores.length; i++) {
@@ -59,3 +88,4 @@ function comecaCronometro() {
 }
 
 comecaCronometro();
+atualizaSituacao ();
